e2f1cd637accb76505208cbbb9375d350835f8ca
AttePel <atte.r.peltonen@gmail.com>
2017-09-21 15:45:51 +0300

    Conditional-statement task 1  done

---
+++ conditional-statements/conditional-statement/conditional-statement/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace conditional_statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("Ohjelma tulostaa positiivinen, negatiivinen tai nolla.");
+		    Console.Writeline("syötä luku: ");
+			string userInput;
+		    userInput = Console.ReadLine();
+
+			int evaluatedNumber;
+			int evaluatedNumber = int.TryParse(userInput, out evaluatedNumber);
+
+			if(evaluatedNumber < 0)
+			{
+			Console.WriteLine($"Numero {evaluatedNumber} on negatiivinen");	
+			}
+			else if {evaluatedNumber >0}
+			{
+				Console.WriteLine($"Numero {evaluatedNumber} on positiivinen");
+			}
+			else
+			{
+				Console.WriteLine($"Numero {evaluatedNumber} on nolla");
+			}
+			Console.WriteLine("Syötit arvon {0}",userInput);
+			Console.WriteLine($"Syötit arvon {userInput}");
+			Console.ReadKey();
+		}
+
