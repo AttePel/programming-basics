87024a305f295f0f2566b558bf06bd53a20cf3a5
AttePel <atte.r.peltonen@gmail.com>
2017-10-23 10:42:40 +0300

    korjaus uudet tiedostot

---
+++ conditional-statements/conditional-statementsNew/task3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Task3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.OutputEncoding = System.Text.Encoding.UTF8;
+			Console.WriteLine("Ohjelma kysyy numeroa ja tulostaa positiivinen, negatiivinen tai nolla, sekä onko luku parillinen vai pariton.");
+			Console.Write("Syötä Luku:");
+			string userInput;
+			userInput = Console.ReadLine();
+
+			int evaluatedNumber;
+			bool isNumber = int.TryParse(userInput, out evaluatedNumber);
+			string isEven = String.Empty;
+			string isPositive;
+
+			if (evaluatedNumber % 2 == 0)
+			{
+				isEven = "parillinen";
+			}
+			else
+			{
+				isEven = "pariton";
+			}
+
+			if (evaluatedNumber > 0)
+			{
+				isPositive = "positiivinen";
+			}
+			else if (evaluatedNumber < 0)
+			{
+				isPositive = "negatiivinen";
+			}
+			else
+			{
+				isPositive = "nolla";
+			}
+
+			Console.WriteLine($"Numero on {isEven} sekä {isPositive}");
+			Console.ReadKey();
+
+		}
+	}
+}
---
+++ conditional-statements/conditional-statementsNew/task5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task6
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+			bool isNumber;
+			int N = 1;
+			int input;
+
+			do
+			{
+				Console.Write("Syötä luku: ");
+
+				if (isNumber = int.TryParse(Console.ReadLine(), out input)) ;
+				{
+					if (input > 0)
+					{
+						for (int i = 1; i <= input; i++)
+						{
+							N = N * i;
+						}
+
+						Console.WriteLine($"{N}");
+						Console.ReadKey();
+					}
+				}
+
+			}
+			while (!isNumber || input < 0);
+		}
+	}
+}
